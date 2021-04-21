d15c04cbdb0a4103c1eb46cff6f8d4ff18453fa3
tonyha <tonytaesung.ha@gmail.com>
2021-04-21 09:29:54 -0700

    fleshed out logic for edge class.

--- lib/graph/edge.rb
+++ lib/graph/edge.rb
@@ -1,5 +1,21 @@
 module Graph
 	class Edge
-		
+		attr_reader :id
+		# store edges for find functionality
+		@@edges = {}
+		@@last_id = 0
+
+		def self.find(id)
+			@@edges[id]
+		end
+
+		def initialize(cost_of_traversal:, start_vertex:, end_vertex:)		
+			@id = @@last_id + 1
+			@cost_of_traversal = cost_of_traversal
+			@start_vertex = start_vertex
+			@end_vertex = end_vertex
+			@pheromone_count = 0
+			@@edges[@id] = self
+		end
 	end
 end
