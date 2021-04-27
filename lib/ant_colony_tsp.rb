6c887bec8e3f8c2d888abec77d5d6edf7b17ecb9
tonyha <tonytaesung.ha@gmail.com>
2021-04-27 07:36:25 -0400

    Initialized ant_cony_tsp spec file.

--- lib/ant_colony_tsp.rb
+++ lib/ant_colony_tsp.rb
@@ -1,3 +1,15 @@
 class AntColonyTsp
-	
+	attr_reader :time
+
+	def initialize(edges:, vertices:)
+		@time = 0
+		
+		edges.each do |edge|
+			
+		end
+
+		vertices.each do |vertex|
+
+		end
+	end	
 end
---
+++ spec/ant_colony_tsp_spec.rb
@@ -0,0 +1,9 @@
+describe AntColonyTsp do
+	it "given a list of vertices and edges, it should convert each vertex input into an Vertex instance" do
+
+	end
+
+	it "given a list of vertices and edges, it should convert each edge input into an Edge instance" do
+		
+	end
+end
