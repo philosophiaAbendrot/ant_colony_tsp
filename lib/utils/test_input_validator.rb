d6723d50fb6132b8b72f2dbab55d915563914d4f
tonyha <tonytaesung.ha@gmail.com>
2021-05-06 19:52:26 -0700

    removed dijkstra pseudocode.

--- lib/utils/test_input_validator.rb
+++ lib/utils/test_input_validator.rb
@@ -1,37 +1,6 @@
 require 'json'
 require 'pqueue'
 
-# https://www.geeksforgeeks.org/dijkstras-shortest-path-algorithm-greedy-algo-7/
-# https://www.youtube.com/watch?v=_lHSawdgXpI
-# pseudocode for Dijkstra's algorithm
-# dijkstra tells us the shortest path from one node to every other node
-# method dijkstra(start_vertex, graph)
-#   create sptSet (shortest path tree set) that keeps track of vertices included in the shortest path tree
-#   create a hash table distance_table which maps vertex id to distance
-#   assign a distance value to all vertices in the input graph. Initialize all distance values are infinite.
-#   assign distance value as 0 for the source vertex so that it is picked first
-#   while sptSet doesn't include all vertices
-#     pick a vertex u which is not in sptSet and has minimum distance value in vertex_distance_values
-# 		add u to sptSet
-#     iterate through all adjacent vertices v of u
-#       if distance value of u + weight of edge u-v is < distance value of v
-#         update distance value of v to be u + weight of edge u-v
-
-# 	let unvisited_nodes be a set of all the vertices in graph minus the start_vertex
-# 	let distances be a hash table mapping vertex ids to distances, set to infinity by default
-# 	let distances[start_node.id] = 0
-#   let queue be a priority queue of edges, prioritizing them such that the edge with the lowest cost of traversal will be extracted when pop is called
-#   push all edges connected to start_node into queue
-
-#   loop while queue is not empty
-#     pop the shortest edge in queue into e
-#     let v be the vertex that is the end vertex of e
-#     remove v from unvisited_nodes
-#     distances[v] = distances[]
-#   end loop
-# end method
-
-
 class TestInputValidator
 	def initialize
 		@vertex_inputs = JSON.parse(File.read(__dir__ + "/test_data/test_vertex_inputs.json"))
@@ -49,55 +18,6 @@ class TestInputValidator
 			@vertex_hash = initialize_vertex_hash
 			@edge_inputs = edge_inputs
 			@edge_mat = initialize_edge_matrix
-		end
-
-		def execute(start_vertex_id)
-			@adj_mat = generate_adjacency_matrix(vertex_inputs, edge_inputs)
-			@dist = {}
-			@prev = {}
-			# queue elements will be edge inputs
-			queue = PQueue.new { |a, b| a[:cost_of_traversal] < b[:cost_of_traversal] }
-			vertices_in_queue = {}
-
-			@vertex_inputs.each do |vertex_input|
-				@dist[vertex_input[:id]] = Float::INFINITY
-				@prev[vertex_input[:id]] = nil
-				queue.push(@edge_mat[start_vertex_id][vertex_input[:id]])
-				vertices_in_queue[vertex_input[:id]] = true
-			end
-
-			@dist[start_vertex_id] = 0
-			
-			while !queue.empty?
-				edge_to_u = queue.pop
-				u = @vertex_hash[edge_to_u[:end_vertex_id]]
-				vertices_in_queue.delete(u[:id])
-
-				# for each neighbour v of u that are still in Q:
-				@edge_mat[u[:id]].select { |val| val != -1 && vertices_in_queue.include?(val) }
-			end
-		end
-
-		private
-
-		def generate_edge_matrix(vertex_inputs, edge_inputs)
-			edge_mat = Array.new(vertex_inputs.length) { Array.new(vertex_inputs.length) { -1 } }
-
-			for edge_input in edge_inputs
-				edge_mat[edge_input[:start_vertex_id]][edge_input[:end_vertex_id]] = edge_input
-			end
-
-			edge_mat
-		end
-
-		def initialize_vertex_hash
-			vertex_hash = {}
-
-			@vertex_inputs.each do |vertex_input|
-				vertex_hash[vertex_input[:id]] = vertex_input
-			end
-
-			vertex_hash
 		end
 	end
 
