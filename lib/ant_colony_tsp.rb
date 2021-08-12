b91dc40b48146cbaa9db6261b13c5b8789cca3c0
Tony Ha <eothel@gmail.com>
2021-08-11 20:42:40 -0700

    Added comments to 'ant_colony_tsp' class.

--- lib/ant_colony_tsp.rb
+++ lib/ant_colony_tsp.rb
@@ -10,19 +10,44 @@ require_relative "graph/vertex"
 require_relative "config"
 require_relative "errors"
 
+# External: Main exectuable class for running this project.
+#   Accepts edges, vertices, and other parameters.
+#   Initializes and configures objects based on inputs.
+#   Runs ant colony optimization logic.
+#   Exports data.
 class AntColonyTsp
-	attr_reader :time
-
+	# Internal: Initializes AntColonyTsp class.
+	# 
+	# edge_inputs - An Array of Hash objects containing information on the
+	#   edges in the graph.
+	# vertex_inputs - An Array of Hash objects containing information on
+	#   vertices in the graph.
+	# include_path_length_vs_iteration - A Boolean object which decides
+	#   whether data on the length of the trail for each iteration is included
+	#   in the output.
+	#
+	# Examples
+	#
+	# edge_inputs = [{ id: 2, start_vertex_id: 3, end_vertex_id: 4,
+    #                  cost_of_traversal: 44.13 },
+    #                { id: 5, start_vertex_id: 9, end_vertex_id: 3,
+    #                  cost_of_traversal: 39.52 }]
+	# vertex_inputs = [{ x_pos: 5.4, y_pos: -3.2, id: 3 },
+	#                  { x_pos: 8.3, y_pos: 6.5, id: 4 },
+	#                  { x_pos: -3.5, y_pos: -5.6, id: 9 }]
+	#
+	# instance = AntColonyTsp.new(edge_inputs: edge_inputs,
+	#                  vertex_inputs:vertex_inputs,
+	#                  include_path_length_vs_iteration: true)
+	#
+	# Returns nothing.
 	def initialize(edge_inputs:, vertex_inputs:,
-								 include_edges_data:,
 								 include_path_length_vs_iteration:)
 
 		@num_vertices = vertex_inputs.length
 		@edge_inputs = edge_inputs
 		@vertex_inputs = vertex_inputs
-		@include_edges_data = include_edges_data
 		@include_path_length_vs_iteration = include_path_length_vs_iteration
-		@time = 0
 
 		config = AntColonyTsp.config
 
@@ -34,38 +59,105 @@ class AntColonyTsp
 		@rand_gen = config.rand_gen
 		@num_iterations = config.num_iterations
 
-		# pass configuration to model classes
+		# Pass configuration to model classes.
 		@edge_class.set_config(config)
 		@graph_class.set_config(config)
 		@ant_class.set_config(config)
 
-		true
+		nil
 	end
 
+	# Internal: Gets Config object associated with class.
+	#
+	# Returns Config object associated with class. If there is no associated
+	#   Config object, it creates a new one.
 	def self.config
 		@@config ||= Config.new
 		@@config
 	end
 
+	# External: Exposes Config object, allowing user to configure it using a
+	#   block.
+	#
+	# &block - A block used to update the Config object.
+	#
+	# Examples
+	#
+	#   AntColonyTsp.configure do |config|
+	#     config.num_ants = 55
+	#   end
+	#
+	# Returns updated Config object.
 	def self.configure(&block)
 		block.call(self.config)
+		# Format the configuration updates.
 		self.config.process_configs
 	end
 
+	# External: Main entry method for this project. Takes in inputs for
+	#   vertices, edges, and other options for output. Runs the ant colony
+	#   optimization algorithm and returns the shortest path.
+	#
+	# edge_inputs - An Array of Hash objects containing information on the
+	#   edges in the graph.
+	# vertex_inputs - An Array of Hash objects containing information on
+	#   vertices in the graph.
+	# include_path_length_vs_iteration - A Boolean object which decides
+	#   whether data on the length of the trail for each iteration is included
+	#   in the output.
+	#
+	# Examples
+	#   edge_inputs = [{ id: 2, start_vertex_id: 3, end_vertex_id: 4,
+    #                  cost_of_traversal: 44.13 },
+    #                  { id: 5, start_vertex_id: 9, end_vertex_id: 3,
+    #                  cost_of_traversal: 39.52 }]
+	#   vertex_inputs = [{ x_pos: 5.4, y_pos: -3.2, id: 3 },
+	#                    { x_pos: 8.3, y_pos: 6.5, id: 4 },
+	#                    { x_pos: -3.5, y_pos: -5.6, id: 9 }]
+	#
+	#   AntColonyTsp.execute(edge_inputs: edge_inputs,
+	#                        vertex_inputs: vertex_inputs,
+	#                        include_path_length_vs_iteration: true)
+ 	# 
+ 	# Returns a Hash object with the following key-value pairs:
+ 	#   vertices - An Array of type Integer holding the ids of the vertices
+ 	#     in order visited.
+ 	#   edges - An Array of type Integer holding the ids of the edges in order
+ 	#     visited.
+ 	#   iteration_path_lengths - An Array of type Float which holds the path
+ 	#     lengths of the shortest trail found on successive iterations
+ 	#     starting with the first iteration. Only included if
+ 	#     'include_path_length_vs_iteration' option is marked true in
+ 	#     'execute' method.
 	def self.execute(edge_inputs:, vertex_inputs:,
-									 include_edges_data: false,
 									 include_path_length_vs_iteration: false)
 		edge_inputs = edge_inputs.map { |el| symbolize_keys(el) }
 		vertex_inputs = vertex_inputs.map { |el| symbolize_keys(el) }
 
-		new(edge_inputs: edge_inputs, vertex_inputs: vertex_inputs, include_edges_data: include_edges_data,
+		new(edge_inputs: edge_inputs, vertex_inputs: vertex_inputs,
 				include_path_length_vs_iteration: include_path_length_vs_iteration).execute
 	end
 
+	# Internal: Takes in inputs for vertices, edges, and other options for
+	#   output. Runs the ant colony optimization algorithm and returns the
+	#   shortest path.
+	#
+	# Returns a Hash object with the following key-value pairs:
+ 	#   vertices - An Array of type Integer holding the ids of the vertices
+ 	#     in order visited.
+ 	#   edges - An Array of type Integer holding the ids of the edges in order
+ 	#     visited.
+ 	#   iteration_path_lengths - An Array of type Float which holds the path
+ 	#     lengths of the shortest trail found on successive iterations
+ 	#     starting with the first iteration. Only included if
+ 	#     'include_path_length_vs_iteration' option is marked true in
+ 	#     'execute' method.
+ 	# Raises PathNotFoundError if there was a failure to find a trail. This
+ 	#   may be because the graph has no Hamiltonian path.
 	def execute
-		# initialize graph
+		# Initialize graph.
 		initialize_graph
-		# initialize ants and place them on random vertices
+		# Initialize ants and place them on random vertices.
 		initialize_ants
 		iteration_count = 0
 		global_shortest_path_vertices = nil
@@ -75,7 +167,7 @@ class AntColonyTsp
 
 		for iteration_count in 0..@num_iterations - 1
 			@ant_class.all.each do |ant|
-				# make every ant execute one tour
+				# Make every ant execute one tour.
 				completed = true
 
 				for i in 0..@num_vertices - 2
@@ -87,18 +179,18 @@ class AntColonyTsp
 					end
 				end
 
-				# move ant back to start position
+				# Move ant back to start position.
 				ant.move_to_start if completed
 			end
 
-			# find ant with shortest path
+			# Find ant with shortest path.
 			ant_with_shortest_path = nil
 			shortest_path_edges = nil
 			shortest_path_length = Float::INFINITY
 			shortest_path_vertices = nil
 
 			@ant_class.all.each do |ant|
-				# if ant path is shorter than the currently shortest path and ant completed a full tour
+				# If ant path is shorter than the currently shortest path and ant completed a full tour.
 				if (path_length = ant.find_path_length) < shortest_path_length && (ant.visited_edge_ids.length == @num_vertices)
 					shortest_path_edges = ant.visited_edge_ids
 					shortest_path_vertices = ant.visited_vertex_ids
@@ -107,17 +199,16 @@ class AntColonyTsp
 				end
 			end
 
-			# lay pheromones on the shortest path
-			# ant_with_shortest_path.lay_pheromones if ant_with_shortest_path
+			# Lay pheromones on the shortest path.
 			ant_with_shortest_path.lay_pheromones if ant_with_shortest_path
 
-			# update trail densities
+			# Update trail densities.
 			@edge_class.update_trail_densities
 
-			# reset ants to original positions
+			# Reset ants to original positions.
 			@ant_class.reset_to_original_position
 
-			# update global shortest path
+			# Update global shortest path.
 			if shortest_path_length < global_shortest_path_length
 				global_shortest_path_length = shortest_path_length
 				global_shortest_path_edges = shortest_path_edges
@@ -132,20 +223,9 @@ class AntColonyTsp
 
 		raise PathNotFoundError.new("Failed to find a tour. The graph may not have a valid path.") if global_shortest_path_length == Float::INFINITY
 
-		if @include_edges_data
-			edges_data = {}
-
-			@edge_class.all.each do |edge|
-				edges_data[edge.id] = { start_vertex_id: edge.start_vertex_id, end_vertex_id: edge.end_vertex_id,
-																cost_of_traversal: edge.cost_of_traversal, trail_density: edge.trail_density }
-			end
-
-			output.merge!(edges_data: edges_data)
-		end
-
 		output.merge!(iteration_path_lengths: iteration_path_lengths) if @include_path_length_vs_iteration
 
-		# clear all database records to prevent application from getting slower with each call
+		# Clear all database records to prevent memory leak with successive calls.
 		@ant_class.destroy_all
 		@edge_class.destroy_all
 		@vertex_class.destroy_all
@@ -155,6 +235,13 @@ class AntColonyTsp
 
 	private
 
+	# Internal: Converts a given hash to an equivalent hash in which all its
+	#   keys are symbols.
+	#
+	# h - A given Hash object.
+	#
+	# Returns a version of the given hash object in which all its keys have
+	#   been converted to symbols.
 	def self.symbolize_keys(h)
 		output = {}
 		h.each do |k, v|
@@ -164,10 +251,17 @@ class AntColonyTsp
 		output
 	end
 
+	# Internal: Initializes a graph using edge and vertex inputs provided on
+	#   invocation of 'execute' method.
+	#
+	# Returns generated graph object.
 	def initialize_graph
 		@graph = @graph_class.new(edge_inputs: @edge_inputs, vertex_inputs: @vertex_inputs)
 	end
 
+	# Internal: Initializes ants and places them on random vertices.
+	#
+	# Returns nothing.
 	def initialize_ants
 		ant_id = 1
 		@num_ants.times do
@@ -175,5 +269,7 @@ class AntColonyTsp
 			@ant_class.new(current_vertex_id: @vertex_class.all[rand_num].id, id: ant_id)
 			ant_id += 1
 		end
+
+		nil
 	end
 end
