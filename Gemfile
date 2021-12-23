e0301bdea2944874e67731e22d05618548a63add
Tony <eothel@gmail.com>
2021-12-23 03:09:12 -0700

    Refactored Ant::Ant class.

--- Gemfile
+++ Gemfile
@@ -12,4 +12,5 @@ end
 
 group :development do
   gem 'rubocop'
+  gem 'pry'
 end
--- lib/ant/ant.rb
+++ lib/ant/ant.rb
@@ -32,55 +32,55 @@ module Ant
       @visited_vertex_ids = [current_vertex_id]
     end
 
-    # Internal: Returns all ants to their original vertices.
-    #
-    # Returns nothing.
-    def self.reset_to_original_position
-      all.each do |ant|
-        first_vertex_id = ant.visited_vertex_ids[0]
-        ant.visited_vertex_ids = [first_vertex_id]
-        ant.visited_edge_ids = []
-        ant.current_vertex_id = first_vertex_id
-      end
-
-      return
-    end
-
-    # Internal: Sets values of configurable class variables.
-    #
-    # config - Object used to configure various classes.
-    #
-    # Returns nothing.
-    def self.set_config(config)
-      # The vertex class.
-      @@vertex_class = config.vertex_class
-
-      # The edge class.
-      @@edge_class = config.edge_class
-
-      # Class used to generate random numbers.
-      @@rand_gen = config.rand_gen
-
+    class << self
+      attr_reader :vertex_class, :edge_class, :rand_gen
       # The amount of pheromone which is deposited by the ant which
       #   finds the shortest trail. This amount of pheromone is divided
       #   evenly among all the edges of the trail.
-      @@q = config.q
-
+      attr_reader :q
       # alpha - coefficient controlling the importance of pheromone strength in
       #   influencing choice of next vertex.
-      @@alpha = config.alpha
+      attr_reader :alpha
       # beta - coefficient controlling the importance of proximity in
       #   influencing choice of next vertex.
-      @@beta = config.beta
+      attr_reader :beta
 
-      return
+      # Internal: Sets values of configurable class variables.
+      #
+      # config - Object used to configure various classes.
+      #
+      # Returns nothing.
+      def set_config(config)
+        @vertex_class = config.vertex_class
+        @edge_class = config.edge_class
+        @rand_gen = config.rand_gen
+        @q = config.q
+        @alpha = config.alpha
+        @beta = config.beta
+
+        nil
+      end
+
+      # Internal: Returns all ants to their original vertices.
+      #
+      # Returns nothing.
+      def reset_to_original_position
+        all.each do |ant|
+          first_vertex_id = ant.visited_vertex_ids[0]
+          ant.visited_vertex_ids = [first_vertex_id]
+          ant.visited_edge_ids = []
+          ant.current_vertex_id = first_vertex_id
+        end
+
+        nil
+      end
     end
 
     # Internal: Return the vertex that the ant object is currently on.
     #
     # Returns the vertex object that the ant is currently on.
     def current_vertex
-      @@vertex_class.find(@current_vertex_id)
+      self.class.vertex_class.find(@current_vertex_id)
     end
 
     # Internal: Returns the x_pos of the vertex that the ant is currently
@@ -106,43 +106,10 @@ module Ant
     #   move. This can occur if there are no outgoing edges to vertice
     #   that haven't been visited, or if all vertices have been visited.
     def move_to_next_vertex
-      # Evaluate preferences.
-      outgoing_edges = current_vertex.outgoing_edge_ids.map { |edge_id| @@edge_class.find(edge_id) }
+      selected_vertex_id, selected_edge_id = find_next_edge_and_vertex
+      return false if selected_vertex_id.nil?
 
-      # Generate a representation of the ant's preferences for each connected vertex.
-      cumulative_preferences = VertexPreferenceGenerator.execute(outgoing_edges: outgoing_edges,
-                                                                 visited_vertex_ids: @visited_vertex_ids.dup,
-                                                                 alpha: @@alpha, beta: @@beta)
-
-      # If there is no option to move to a vertex, terminate early and
-      #   return false to indicate that no movement occurred.
-      return false if cumulative_preferences.empty?
-
-      # Use a random number to randomly select a vertex to move to based
-      #   on the generated preferences.
-      rand_num = @@rand_gen.rand_float
-
-      selected_vertex_id = nil
-
-      (0..cumulative_preferences.length - 1).each do |i|
-        vertex_id, cumulative_probability = cumulative_preferences[i]
-
-        if cumulative_probability >= rand_num
-          selected_vertex_id = vertex_id
-          break
-        end
-      end
-
-      # Find the edge that the ant must travel through to travel to the
-      #   new vertex.
-      selected_edge_id = outgoing_edges.select { |edge| edge.end_vertex_id == selected_vertex_id }.first.id
-
-      # Move to new vertex.
-      @current_vertex_id = selected_vertex_id
-      @visited_vertex_ids << selected_vertex_id
-      @visited_edge_ids << selected_edge_id
-
-      # Return true to indicate that ant has moved successfully.
+      move_to_vertex_through_edge(selected_vertex_id, selected_edge_id)
       true
     end
 
@@ -155,19 +122,10 @@ module Ant
     #   vertex and false otherwise.
     def move_to_start
       start_vertex_id = @visited_vertex_ids[0]
+      selected_edge_id = find_edge_to_start_vertex(start_vertex_id)
+      return false if selected_edge_id.nil?
 
-      outgoing_edges = current_vertex.outgoing_edge_ids.map { |edge_id| @@edge_class.find(edge_id) }
-
-      prospective_edges = outgoing_edges.select { |edge| edge.end_vertex_id == start_vertex_id }
-
-      return false if prospective_edges.empty?
-
-      selected_edge_id = prospective_edges.first.id
-
-      @current_vertex_id = start_vertex_id
-      @visited_vertex_ids << start_vertex_id
-      @visited_edge_ids << selected_edge_id
-
+      move_to_vertex_through_edge(start_vertex_id, selected_edge_id)
       true
     end
 
@@ -176,19 +134,86 @@ module Ant
     #
     # Returns the total travel length.
     def find_path_length
-      @visited_edge_ids.map { |el| @@edge_class.find(el).cost_of_traversal }.sum
+      @visited_edge_ids.map { |el| self.class.edge_class.find(el).cost_of_traversal }.sum
     end
 
     # Internal: Adds pheremones to all the edges that the ant travelled
     #   through on its path.
     def lay_pheromones
-      trail_density = @@q / find_path_length
+      trail_density = self.class.q / find_path_length
 
-      @visited_edge_ids.map { |el| @@edge_class.find(el) }.each do |edge|
+      visited_edges = @visited_edge_ids.map do |edge_id|
+        self.class.edge_class.find(edge_id)
+      end
+
+      visited_edges.each do |edge|
         edge.add_pheromones(trail_density)
       end
 
-      return
+      nil
+    end
+
+    private
+
+    def evaluate_preferences
+      VertexPreferenceGenerator
+        .execute(outgoing_edges: outgoing_edges,
+                 visited_vertex_ids: @visited_vertex_ids.dup,
+                 alpha: self.class.alpha,
+                 beta: self.class.beta)
+    end
+
+    def select_rand_vertex(cumulative_preferences)
+      rand_num = self.class.rand_gen.rand_float
+      selected_vertex_id = nil
+
+      (0..cumulative_preferences.length - 1).each do |i|
+        vertex_id, cumulative_probability = cumulative_preferences[i]
+
+        if cumulative_probability >= rand_num
+          selected_vertex_id = vertex_id
+          break
+        end
+      end
+
+      selected_vertex_id
+    end
+
+    def find_connecting_edge(selected_vertex_id)
+      outgoing_edges.select do |edge|
+        edge.end_vertex_id == selected_vertex_id
+      end.first.id
+    end
+
+    def find_next_edge_and_vertex
+      cumulative_preferences = evaluate_preferences
+      return [nil, nil] if cumulative_preferences.empty?
+
+      selected_vertex_id = select_rand_vertex(cumulative_preferences)
+      selected_edge_id = find_connecting_edge(selected_vertex_id)
+      [selected_vertex_id, selected_edge_id]
+    end
+
+    def move_to_vertex_through_edge(selected_vertex_id, selected_edge_id)
+      @current_vertex_id = selected_vertex_id
+      @visited_vertex_ids << selected_vertex_id
+      @visited_edge_ids << selected_edge_id
+
+      nil
+    end
+
+    def outgoing_edges
+      current_vertex.outgoing_edge_ids.map do |edge_id|
+        self.class.edge_class.find(edge_id)
+      end
+    end
+
+    def find_edge_to_start_vertex(start_vertex_id)
+      prospective_edges = outgoing_edges.select do |edge|
+        edge.end_vertex_id == start_vertex_id
+      end
+
+      prospective_edges.first&.id
     end
   end
 end
