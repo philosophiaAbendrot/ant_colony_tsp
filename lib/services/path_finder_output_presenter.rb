ae6fbf8a05ea9754c283a5024a45c3032dbe255f
Tony <eothel@gmail.com>
2021-12-30 02:36:44 -0700

    Linted PathFinderOutputPresenter class.

--- lib/services/path_finder_output_presenter.rb
+++ lib/services/path_finder_output_presenter.rb
@@ -1,7 +1,9 @@
 class PathFinderOutputPresenter
+  attr_reader :formatted_hash
+
   def initialize(path_finder_goal_seek, options = {})
     @path_finder_goal_seek = path_finder_goal_seek
-    generate_formatted_hash
+    @formatted_hash = generate_formatted_hash
 
     if options[:include_path_length_vs_iteration] &&
        @path_finder_goal_seek.iteration_path_lengths
@@ -9,20 +11,14 @@ class PathFinderOutputPresenter
     end
   end
 
-  def formatted_hash
-    @formatted_hash.dup
-  end
-
   private
 
   def generate_formatted_hash
-    @formatted_hash = {
+    {
       vertices:    @path_finder_goal_seek.shortest_path_vertices,
       edges:       @path_finder_goal_seek.shortest_path_edges,
       path_length: @path_finder_goal_seek.shortest_path_length
     }
-
-    nil
   end
 
   def incorporate_iteration_path_lengths
