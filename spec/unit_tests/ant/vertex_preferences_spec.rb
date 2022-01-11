99edafbb939be7673df23962f65decbc78618f17
Tony <eothel@gmail.com>
2022-01-11 02:07:44 -0700

    Rewrote specs for VertexPreferencesSpec.

--- spec/unit_tests/ant/vertex_preferences_spec.rb
+++ spec/unit_tests/ant/vertex_preferences_spec.rb
@@ -4,154 +4,172 @@ require 'spec_helper'
 
 describe Ant::VertexPreferences do
   include GeneratorHelpers
-  let(:vertex_inputs) do
-    [{ id: 1, x_pos: 5.3, y_pos: 8.9 },
-     { id: 2, x_pos: -8.4, y_pos: 7.2 },
-     { id: 3, x_pos: -4, y_pos: -6 },
-     { id: 4, x_pos: 9.5, y_pos: 5 }]
+
+  let(:vertex1) do
+    instance_double(Graph::Vertex, id: 1, x_pos: 5.3, y_pos: 8.9)
   end
-  let(:edge_inputs) do
-    [{ id: 1, cost_of_traversal: 4.6, start_vertex_id: 1, end_vertex_id: 3 },
-     { id: 2, cost_of_traversal: 9.5, start_vertex_id: 1, end_vertex_id: 4 },
-     { id: 3, cost_of_traversal: 7.3, start_vertex_id: 1, end_vertex_id: 2 }]
+  let(:vertex2) do
+    instance_double(Graph::Vertex, id: 2, x_pos: -8.4, y_pos: 7.2)
+  end
+  let(:vertex3) do
+    instance_double(Graph::Vertex, id: 3, x_pos: -4, y_pos: -6)
+  end
+  let(:vertex4) do
+    instance_double(Graph::Vertex, id: 4, x_pos: 9.5, y_pos: 5)
+  end
+  let(:vertex_class) do
+    class_double('Graph::Vertex')
+  end
+
+  let(:edge1) do
+    instance_double(
+      Graph::Edge, id: 1, cost_of_traversal: 4.6,
+      start_vertex_id: 1, end_vertex_id: 3,
+      trail_density: 0.3
+    )
+  end
+  let(:edge2) do
+    instance_double(
+      Graph::Edge, id: 2, cost_of_traversal: 9.5,
+      start_vertex_id: 1, end_vertex_id: 4,
+      trail_density: 0.12
+    )
+  end
+  let(:edge3) do
+    instance_double(
+      Graph::Edge, id: 3, cost_of_traversal: 7.3,
+      start_vertex_id: 1, end_vertex_id: 2,
+      trail_density: 0.54
+    )
+  end
+  let(:edge_class) do
+    class_double('Graph::Edge')
   end
 
   let(:default_pheromone_density) { 3 }
   let(:config) { Config.new }
+  let(:fixed_rand_value) { 0.7 }
 
-  before(:each) do
-    generate_vertices(vertex_inputs)
-    generate_edges(edge_inputs, default_pheromone_density)
+  let(:preference) do
+    described_class.new(
+      visited_vertex_ids: visited_vertex_ids,
+      outgoing_edges:     [edge1, edge2, edge3],
+      alpha:              config.alpha,
+      beta:               config.beta
+    )
   end
 
-  after(:each) do
-    Graph::Vertex.destroy_all
-    Graph::Edge.destroy_all
+  before do
+    allow(vertex_class).to receive(:find).with(1).and_return(vertex1)
+    allow(vertex_class).to receive(:find).with(2).and_return(vertex2)
+    allow(vertex_class).to receive(:find).with(3).and_return(vertex3)
+    allow(vertex_class).to receive(:find).with(4).and_return(vertex4)
+    stub_const('Graph::Vertex', vertex_class)
+
+    allow(edge_class).to receive(:find).with(1).and_return(edge1)
+    allow(edge_class).to receive(:find).with(2).and_return(edge2)
+    allow(edge_class).to receive(:find).with(3).and_return(edge3)
+    stub_const('Graph::Edge', edge_class)
+
+    allow(preference).to receive(:rand).and_return(fixed_rand_value)
   end
 
-  context 'when none of the vertices have been visited (except the current vertex)' do
-    let(:visited_vertex_ids) { [1] }
+  describe '#select_rand_vertex' do
+    subject(:selected_vertex) do
+      preference.select_rand_vertex
+    end
 
-    it 'should provide a mapping of vertex ids to preference value' do
-      edge_1 = Graph::Edge.find(1)
-      tau_1_3 = edge_1.trail_density**config.alpha
-      eta_1_3 = (1 / edge_1.cost_of_traversal)**config.beta
+    context 'when none of the vertices have been visited'\
+            '(except the current vertex)' do
+      let(:visited_vertex_ids) { [1] }
 
-      edge_2 = Graph::Edge.find(2)
-      tau_1_4 = edge_2.trail_density**config.alpha
-      eta_1_4 = (1 / edge_2.cost_of_traversal)**config.beta
+      context 'when rand returns 0.7' do
+        let(:fixed_rand_value) { 0.7 }
 
-      edge_3 = Graph::Edge.find(3)
-      tau_1_2 = edge_3.trail_density**config.alpha
-      eta_1_2 = (1 / edge_3.cost_of_traversal)**config.beta
+        it { is_expected.to eq(2) }
+      end
 
-      sum = (tau_1_3 * eta_1_3 + tau_1_4 * eta_1_4 + tau_1_2 * eta_1_2).to_f
+      context 'when rand returns 0.4' do
+        let(:fixed_rand_value) { 0.5 }
 
-      hashed_result = { edge_1.end_vertex_id => tau_1_3 * eta_1_3 / sum, edge_2.end_vertex_id => tau_1_4 * eta_1_4 / sum,
-                        edge_3.end_vertex_id => tau_1_2 * eta_1_2 / sum }
-      cumulative_probability_mapping = []
-      cumulative_prob = 0
-      cumulative_prob += hashed_result[edge_1.end_vertex_id]
-      cumulative_probability_mapping << [edge_1.end_vertex_id, cumulative_prob]
+        it { is_expected.to eq(4) }
+      end
 
-      cumulative_prob += hashed_result[edge_2.end_vertex_id]
-      cumulative_probability_mapping << [edge_2.end_vertex_id, cumulative_prob]
+      context 'when rand returns 0.1' do
+        let(:fixed_rand_value) { 0.3 }
 
-      cumulative_prob += hashed_result[edge_3.end_vertex_id]
-      cumulative_probability_mapping << [edge_3.end_vertex_id, cumulative_prob]
+        it { is_expected.to eq(3) }
+      end
+    end
 
-      preferences = Ant::VertexPreferences.new(
-        visited_vertex_ids: visited_vertex_ids,
-        outgoing_edges:     Graph::Edge.all,
-        alpha:              config.alpha,
-        beta:               config.beta,
-      )
+    context 'when some vertices have been visited' do
+      let(:visited_vertex_ids) { [1, 3] }
 
-      expect(
-        compare_array_of_floats(
-          preferences.preference_mapping,
-          cumulative_probability_mapping
-        )
-      ).to be true
+      context 'when rand returns 0.7' do
+        let(:fixed_rand_value) { 0.7 }
+
+        it { is_expected.to eq 2 }
+      end
+
+      context 'when rand returns 0.4' do
+        let(:fixed_rand_value) { 0.4 }
+
+        it { is_expected.to eq 2 }
+      end
+
+      context 'when rand returns 0.1' do
+        let(:fixed_rand_value) { 0.1 }
+
+        it { is_expected.to eq 4 }
+      end
+    end
+
+    context 'when all vertices have been visited' do
+      let(:visited_vertex_ids) { [1, 2, 3, 4] }
+
+      context 'when rand returns 0.7' do
+        let(:fixed_rand_value) { 0.7 }
+
+        it { is_expected.to be nil }
+      end
+
+      context 'when rand returns 0.4' do
+        let(:fixed_rand_value) { 0.4 }
+
+        it { is_expected.to be nil }
+      end
+
+      context 'when rand returns 0.1' do
+        let(:fixed_rand_value) { 0.1 }
+
+        it { is_expected.to be nil }
+      end
     end
   end
 
-  context 'when some vertices have been visited' do
-    let(:visited_vertex_ids) { [1, 3] }
+  describe '#empty?' do
+    subject { preference.empty? }
 
-    it 'should provided a mapping of vertex ids to preference value' do
-      edge_2 = Graph::Edge.find(2)
-      tau_1_4 = edge_2.trail_density**config.alpha
-      eta_1_4 = (1 / edge_2.cost_of_traversal)**config.beta
-      product_1_4 = tau_1_4 * eta_1_4
+    context 'when all vertices have been visited' do
+      let(:visited_vertex_ids) { [1, 2, 3, 4] }
 
-      edge_3 = Graph::Edge.find(3)
-      tau_1_2 = edge_3.trail_density**config.alpha
-      eta_1_2 = (1 / edge_3.cost_of_traversal)**config.beta
-      product_1_2 = tau_1_2 * eta_1_2
+      context 'when rand returns 0.7' do
+        let(:fixed_rand_value) { 0.7 }
 
-      sum = (product_1_4 + product_1_2).to_f
+        it { is_expected.to be true }
+      end
 
-      hashed_result = { edge_2.end_vertex_id => tau_1_4 * eta_1_4 / sum,
-                        edge_3.end_vertex_id => tau_1_2 * eta_1_2 / sum }
-      cumulative_probability_mapping = []
-      cumulative_prob = 0
+      context 'when rand returns 0.4' do
+        let(:fixed_rand_value) { 0.4 }
 
-      cumulative_prob += hashed_result[edge_2.end_vertex_id]
-      cumulative_probability_mapping << [edge_2.end_vertex_id, cumulative_prob]
+        it { is_expected.to be true }
+      end
 
-      cumulative_prob += hashed_result[edge_3.end_vertex_id]
-      cumulative_probability_mapping << [edge_3.end_vertex_id, cumulative_prob]
+      context 'when rand returns 0.1' do
+        let(:fixed_rand_value) { 0.1 }
 
-      preferences = Ant::VertexPreferences.new(
-        visited_vertex_ids: visited_vertex_ids,
-        outgoing_edges:     Graph::Edge.all,
-        alpha:              config.alpha,
-        beta:               config.beta
-      )
-
-      expect(compare_array_of_floats(preferences.preference_mapping,
-                                     cumulative_probability_mapping)).to be true
+        it { is_expected.to be true }
+      end
     end
-  end
-
-  context 'when all vertices have been visited' do
-    let(:visited_vertex_ids) { [1, 2, 3, 4] }
-
-    it 'should return an empty array' do
-      preferences = Ant::VertexPreferences.new(
-        visited_vertex_ids: visited_vertex_ids,
-        outgoing_edges:     Graph::Edge.all,
-        alpha:              config.alpha,
-        beta:               config.beta,
-      )
-
-      expect(preferences.preference_mapping).to eq([])
-    end
-  end
-
-  private
-
-  def compare_array_of_floats(arr1, arr2)
-    return false if arr1.length != arr2.length
-
-    (0..arr1.length - 1).each do |i|
-      sub_arr1 = arr1[i]
-      sub_arr2 = arr2[i]
-
-      return false unless sub_arr1.length == sub_arr2.length
-
-      return false unless compare_floats(arr1[i][0], arr2[i][0]) && compare_floats(arr1[i][1], arr2[i][1])
-    end
-
-    true
-  end
-
-  def compare_floats(float1, float2)
-    # if percent difference is < 0.1%, pass
-    diff = (float1 - float2).abs
-    average = (float1 + float2) / 2.0
-    percent_diff = diff / average * 100
-    percent_diff < 0.1
   end
 end
